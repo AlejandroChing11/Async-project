87f70333dcbc53893aa85c7164d7ed979c4eaad3
AlejandroChing11 <alejandroching2004@hotmail.com>
2022-10-08 15:26:36 -0500

    Adding ig posts

--- src/assets/main.js
+++ src/assets/main.js
@@ -18,7 +18,23 @@ async function fechData(urlApi) {
 (async () => {
     try {
         const post = await fetchData(API);
-        
+        let view = `
+        ${post.edges.map(post => `
+            <div class="group relative">
+                <div
+                class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
+                    <img src="" alt="" class="w-full">
+                </div>
+                <div class="mt-4 flex justify-between">
+                    <h3 class="text-sm text-gray-700">
+                        <span aria-hidden="true" class="absolute inset-0"></span>
+                    title
+                    </h3>
+                </div>
+            </div>
+        `)}
+
+        `;
     } catch (e) {
 
     }
