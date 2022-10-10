7ca0fbf8d17693acacb59d0ea6b45f3dfeed38ac
AlejandroChing11 <alejandroching2004@hotmail.com>
2022-10-10 18:41:08 -0500

    Adding API correctly

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,6 +1,6 @@
 const API = 'https://youtube-v31.p.rapidapi.com/search?channelId=UCX9NJ471o7Wie1DQe94RVIg&part=snippet%2Cid&order=date&maxResults=50';
 
-
+const content = null || document.getElementById('content');
 
 const options = {
 	method: 'GET',
@@ -17,5 +17,27 @@ async function fetchData(urlApi) {
 };
 
 (async () => {
-    
+    try {
+        const videos = await fetchData(API);
+        let view = `
+        ${videos.items.map(video => `
+        <div class="group relative">
+            <div
+            class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
+                <img src="${video.snippet.thumbnail.high.url}" alt="${video.snippet.description}" class="w-full">
+            </div>
+            <div class="mt-4 flex justify-between">
+                <h3 class="text-sm text-gray-700">
+                    <span aria-hidden="true" class="absolute inset-0"></span>
+                    ${video.snippet.title}
+                </h3>
+            </div>
+        </div>
+        `)}
+
+        `;
+    } catch {
+
+    }
+
 })();
