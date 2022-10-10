98bc77bae3ed82f28c373c5c19976cc60bdecee6
AlejandroChing11 <alejandroching2004@hotmail.com>
2022-10-10 18:30:52 -0500

    Youtube API

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,41 +1,21 @@
-const API = 'https://instagram188.p.rapidapi.com/userpost/2117295831/12/%7Bend_cursor%7D';
+const API = 'https://youtube-v31.p.rapidapi.com/search?channelId=UCX9NJ471o7Wie1DQe94RVIg&part=snippet%2Cid&order=date&maxResults=50';
+
 
 
 const options = {
 	method: 'GET',
 	headers: {
 		'X-RapidAPI-Key': '7ca582198bmsh267140fbf4b34b9p1acda6jsn201e8600c0d1',
-		'X-RapidAPI-Host': 'instagram188.p.rapidapi.com'
+		'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
 	}
 };
 
-async function fechData(urlApi) {
+async function fetchData(urlApi) {
     const reponse = await fetch(urlApi, options);
     const data = await reponse.json();
     return data;
-}
+};
 
 (async () => {
-    try {
-        const post = await fetchData(API);
-        let view = `
-        ${post.edges.map(post => `
-            <div class="group relative">
-                <div
-                class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
-                    <img src="" alt="" class="w-full">
-                </div>
-                <div class="mt-4 flex justify-between">
-                    <h3 class="text-sm text-gray-700">
-                        <span aria-hidden="true" class="absolute inset-0"></span>
-                    title
-                    </h3>
-                </div>
-            </div>
-        `)}
-
-        `;
-    } catch (e) {
-
-    }
+    
 })();
